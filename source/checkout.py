9316ccf6b946c7d0dfea4fb0b836b3d273870994
amansin0504 <singh.amandeep@hotmail.co.in>
2022-07-29 15:56:29 -0400

    first commit

---
+++ source/checkout.py
@@ -0,0 +1,14 @@
+import random
+import re
+import sys
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/checkout')
+def ad():
+    return render_template('checkout.json')
+
+if __name__ == '__main__':
+    app.run(host='localhost',port=5002,debug=True)
