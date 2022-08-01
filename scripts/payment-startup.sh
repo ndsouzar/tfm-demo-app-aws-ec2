30eff94412e3697db2a36317291a23c3f4909d3f
amansin0504 <singh.amandeep@hotmail.co.in>
2022-08-01 19:00:29 -0400

    del files

--- scripts/payment-startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install python3-pip -y
-sudo apt install wget
-sudo pip install Flask
-
-git clone https://github.com/amansin0504/csw-vm-demo.git
-cd csw-vm-demo/source/
-python3 payment.py &
